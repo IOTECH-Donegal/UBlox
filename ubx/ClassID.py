9a17b269a39b18e7cc227767d9207294cce9ad40
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2021-08-29 12:34:44 +0100

    Initial version

---
+++ ubx/ClassID.py
@@ -0,0 +1,7 @@
+'''
+Extracted from ZED-F9P - Interface Description, section 3
+'''
+
+UBX_CLASS = {
+    b"\x01": "NAV",  # Navigation solution messages
+}
---
+++ ubx/MessageID.py
@@ -0,0 +1,5 @@
+UBX_NAV = {
+    b"\x01": "NAV-POSECEF",
+    b"\x02": "NAV-POSLLH",
+    b"\x3c": "NAV-RELPOSNED"
+}
