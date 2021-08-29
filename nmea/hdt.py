32f7ad5c8226347e1cdd28f1a63336831cf4d34a
InishowenTechnology Ltd <58892653+IOTECH-Donegal@users.noreply.github.com>
2021-08-29 12:29:46 +0100

    Initial version

---
+++ nmea/hdt.py
@@ -0,0 +1,7 @@
+# HDT - Heading - True
+# Actual vessel heading in degrees true produced by any device or system producing true heading.
+# $--HDT,x.x,T*hh<CR><LF>
+# Heading, degrees True
+# T = True
+# Checksum
+# Example: $GPHDT,274.07,T*03
