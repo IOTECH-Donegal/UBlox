7fe285525aa09e48663e71710715de91b60c978a
John ORaw <john.oraw@iotech.ie>
2022-01-07 09:26:40 +0000

    Added sensor

--- ubx/Sensors.py
+++ ubx/Sensors.py
@@ -1,5 +1,5 @@
 UBLOX = {
-    "1234567": "ZED F9-P MovingBase",
+    "5B4C9A8C5C": "ZED F9-P Moving Base",
     "1949258": "ZED F9-P Heading",
     "3456789": "ZED F9-P Roll",
     "5A4C6ACEDB": "ZED F9-R Heading and INS"
